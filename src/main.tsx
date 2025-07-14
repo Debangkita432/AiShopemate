59ec22191f17e355a6a17810cc56ea3309551d45
subhamsoni858 <121454449+subhamsoni858@users.noreply.github.com>
2025-07-14 14:00:17 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { BudgetProvider } from "./context/BudgetContext";
+
+createRoot(document.getElementById("root")!).render(
+  <BudgetProvider>
+    <App />
+  </BudgetProvider>
+);
